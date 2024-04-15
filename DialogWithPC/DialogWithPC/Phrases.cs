b6b86e54d7f75cbc9c30ddc386c3e87c755842a4
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-15 23:45:58 +0700

    Phrases_ClassCreation_Test success
    был создан класс Phrases

---
+++ DialogWithPC/DialogWithPC/Phrases.cs
@@ -0,0 +1,14 @@
+﻿namespace DialogWithPCTests
+{
+    public class Phrases
+    {
+        private string positivePhrase;
+        private string negativePhrase;
+
+        public Phrases(string v1, string v2)
+        {
+            this.positivePhrase = v1;
+            this.negativePhrase = v2;
+        }
+    }
+}
