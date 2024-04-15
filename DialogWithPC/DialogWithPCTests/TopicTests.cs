be23f3dcfb0e64f66b7a3fce9b1a44c5ca656ffc
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-15 23:54:26 +0700

    Topic_ClassCreation_Test failed
    не существует класса Topic. необходимо его создать

---
+++ DialogWithPC/DialogWithPCTests/TopicTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogWithPCTests
+{
+    [TestClass]
+    public class TopicTests
+    {
+        [TestMethod]
+        public void Topic_ClassCreation_Test()
+        {
+            Topic topic = new Topic("Путешествия",new List<Phrases>());
+            Assert.IsNotNull(topic);
+        }
+    }
+}
