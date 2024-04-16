f780dee36d107debf37ea1e5c181f9338c03e44c
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-16 13:39:54 +0700

    Dialog_ClassCreation_Test success

---
+++ DialogWithPC/DialogWithPC/Dialog.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DialogWithPCTests
+{
+    public class Dialog
+    {
+        private List<Topic> topics;
+        private Computer computer;
+
+        public Dialog(List<Topic> topics, Computer computer)
+        {
+            this.topics = topics;
+            this.computer = computer;
+        }
+    }
+}
