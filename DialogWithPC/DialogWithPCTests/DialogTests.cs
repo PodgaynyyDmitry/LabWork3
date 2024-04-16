3854d9225973e9723af802ba60dd19d5465747fd
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-16 13:38:47 +0700

    Dialog_ClassCreation_Test failed

---
+++ DialogWithPC/DialogWithPCTests/DialogTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogWithPCTests
+{
+    [TestClass]
+    public class DialogTests
+    {
+        [TestMethod]
+        public void Dialog_ClassCreation_Test() 
+        {
+            List<Topic> topics = new List<Topic>();
+            Computer computer = new Computer();
+            Dialog dialog = new Dialog(topics, computer);
+            Assert.IsNotNull(dialog);
+        }
+    }
+}
