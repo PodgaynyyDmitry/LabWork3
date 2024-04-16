d39d8654571d658794b3ee07c3158c126cce9155
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-16 10:36:05 +0700

    Computer_ClassCreation_Test success
    был создан класс Computer

---
+++ DialogWithPC/DialogWithPC/Computer.cs
@@ -0,0 +1,14 @@
+﻿namespace DialogWithPCTests
+{
+    public class Computer
+    {
+        private Topic currentTopic;
+        private AnswerHandler answerHandler;
+        private string currentPhrase;
+
+        public Computer()
+        {
+            this.answerHandler = new AnswerHandler();
+        }
+    }
+}
