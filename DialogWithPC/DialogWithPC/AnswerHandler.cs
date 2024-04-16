bd6a3528c3710216b9bfce0adbbdf8ffc70c3b02
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-16 09:37:49 +0700

    AnswerHandler_CheckDialogFinishStatus_True_Test success
    была дореализована логика работы метода

--- DialogWithPC/DialogWithPC/AnswerHandler.cs
+++ DialogWithPC/DialogWithPC/AnswerHandler.cs
@@ -3,8 +3,14 @@ namespace DialogWithPCTests
 {
     public class AnswerHandler
     {
+        private string[] finishDialogPhrases = {"Я хочу завершить диалог","Я хочу закончить беседу","Я наговорился","Мне надоело"};
         public bool CheckDialogFinishStatus(string v)
         {
+            foreach (string phrase in finishDialogPhrases)
+            {
+                if (phrase == v)
+                    return true;
+            }
             return false;
         }
     }
