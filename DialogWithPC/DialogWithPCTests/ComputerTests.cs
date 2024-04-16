d0b43e74be53300f7d43dc1a022a15af97ae7be9
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-16 10:32:37 +0700

    Computer_ClassCreation_Test failed
    класс Computer не существует

---
+++ DialogWithPC/DialogWithPCTests/ComputerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogWithPCTests
+{
+    [TestClass]
+    public class ComputerTests
+    {
+        [TestMethod]
+        public void Computer_ClassCreation_Test()
+        {
+            Computer computer = new Computer();
+            Assert.IsNotNull(computer);
+        }
+    }
+}
