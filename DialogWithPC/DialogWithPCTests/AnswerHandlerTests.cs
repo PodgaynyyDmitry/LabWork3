589efd53630b4827f41fa518d502aa91ff02ecb5
PodgaynyyDmitry <116504105+PodgaynyyDmitry@users.noreply.github.com>
2024-04-16 09:20:38 +0700

    AnswerHandler_ClassCreatin_Test failed
    не существует класса AnswerHandler. Необходимо его реализовать

---
+++ DialogWithPC/DialogWithPCTests/AnswerHandlerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogWithPCTests
+{
+    [TestClass]
+    public class AnswerHandlerTests
+    {
+        [TestMethod]
+        public void AnswerHandler_ClassCreatin_Test()
+        {
+            AnswerHandler answerHandler = new AnswerHandler();
+            Assert.IsNotNull(answerHandler);
+        }
+    }
+}
